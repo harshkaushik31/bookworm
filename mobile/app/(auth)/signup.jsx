7ea36bc46b1d3afb0077fabef55f4b54a44036d0
Harsh Kaushik <kaushikyuvraj134@gmail.com>
2025-12-26 02:06:59 +0530

    feat/added-zustad-for-authStorage

--- mobile/app/(auth)/signup.jsx
+++ mobile/app/(auth)/signup.jsx
@@ -1,9 +1,10 @@
 import styles from '@/assets/styles/signup.styles'
-import { View, KeyboardAvoidingView, Platform, Text, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native'
+import { View, KeyboardAvoidingView, Platform, Text, TextInput, TouchableOpacity, ActivityIndicator, Alert } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import COLORS from '@/constants/colors'
 import { useState } from 'react'
 import { useRouter } from 'expo-router'
+import { useAuthStore } from '@/store/authStore'
 
 export default function Signup() {
 
@@ -11,12 +12,16 @@ export default function Signup() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [showPassword, setShowPassword] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
+    const { user, isLoading, register } = useAuthStore();
+
+    console.log('User is here: ',user)
 
     const router = useRouter()
 
     const handleSignUp = async () => {
-
+        const result = await register(username, email, password);
+        
+        if(!result.success) Alert.alert("Error ",result.error)
     }
 
     return (
